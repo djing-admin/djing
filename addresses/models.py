d4e8e53283c122b64d680bd208350250633b6838
Krunal Dodiya <kunal.dodiya1@gmail.com>
2025-01-07 17:19:46 +0530

    init

--- addresses/models.py
+++ addresses/models.py
@@ -1,9 +1,5 @@
 from django.db import models
 
-from django.contrib.auth import get_user_model
-
-UserModel = get_user_model()
-
 
 class Address(models.Model):
     id = models.AutoField(primary_key=True)
@@ -16,12 +12,12 @@ class Address(models.Model):
     state = models.CharField(max_length=255)
     country = models.CharField(max_length=255)
 
-    user = models.ForeignKey(
-        UserModel,
+    user = models.OneToOneField(
+        "Address",
         on_delete=models.CASCADE,
-        blank=True,
         null=True,
-        related_name="user",
+        blank=True,
+        related_name="address",
     )
 
     created_at = models.DateTimeField(auto_now_add=True)
