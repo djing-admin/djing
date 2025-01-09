6fb7ac01beef9c2999c1811a9e0d8843ba679368
Krunal Dodiya <kunal.dodiya1@gmail.com>
2025-01-09 12:05:11 +0530

    init

--- djing_ui/src/fields/Detail/HasOneField.vue
+++ djing_ui/src/fields/Detail/HasOneField.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
 import { useFetchRelatedResources } from "@/composables/useFetchRelatedResources";
-import { onMounted } from "vue";
+import { isNil } from "lodash";
+import { onMounted, ref } from "vue";
 
 const { fetchRelatedResources } = useFetchRelatedResources();
 
@@ -33,18 +34,26 @@ onMounted(async () => {
   }
 });
 
+const related_resource = ref();
+const related_resource_panels = ref();
+const related_resource_title = ref();
+
 const handleFetchRelatedResources = async () => {
   try {
     const data = await fetchRelatedResources(field);
 
-    console.log({ data });
-
-    // data.panels.forEach((panel: any) => {
-    //   resourceDetailStore.data.panels.push(panel);
-    // });
+    related_resource.value = data.resource;
+    related_resource_panels.value = data.panels;
+    related_resource_title.value = data.title;
   } catch (error: any) {
     Djing.error(error.response.data.data);
   }
+};
+
+const resolve_component_name = (panel: any) => {
+  return isNil(panel.prefix_component) || panel.prefix_component
+    ? "detail-" + panel.component
+    : panel.component;
 };
 </script>
 
@@ -56,6 +65,23 @@ const handleFetchRelatedResources = async () => {
       :singular_name="field.singular_label"
     />
 
-    <div v-else>hello</div>
+    <div v-else>
+      <div v-for="panel in related_resource_panels">
+        <Card class="-m-6 p-6 divide-y divide-gray-100 dark:divide-gray-700">
+          <component
+            v-for="(field, index) in panel.fields"
+            :key="index"
+            :index="index"
+            :is="resolve_component_name(field)"
+            :resource="resource"
+            :resource_name="resource_name"
+            :resource_id="resource_id"
+            :field="field"
+          >
+            {{ resolve_component_name(field) }}
+          </component>
+        </Card>
+      </div>
+    </div>
   </div>
 </template>
